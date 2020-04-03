a3d8a5ba277ba13d696b11135d1a6c3547ca5ce8
Oz2020 <59335918+Oz2020@users.noreply.github.com>
2020-04-03 18:00:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 
 	"golang.org/x/net/websocket"
+	"cloud.google.com/go/pubsub"
 )
 
 type headers []string
@@ -111,6 +112,31 @@ func read(ws *websocket.Conn) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		publish('crawler-249816','bwin-events',msg[:n])
 		fmt.Printf("<< %s\n", msg[:n])
 	}
+}
+
+func publish(w io.Writer, projectID, topicID, msg string) error {
+        // projectID := "my-project-id"
+        // topicID := "my-topic"
+        // msg := "Hello World"
+        ctx := context.Background()
+        client, err := pubsub.NewClient(ctx, projectID)
+        if err != nil {
+                return fmt.Errorf("pubsub.NewClient: %v", err)
+        }
+
+        t := client.Topic(topicID)
+        result := t.Publish(ctx, &pubsub.Message{
+                Data: []byte(msg),
+        })
+        // Block until the result is returned and a server-generated
+        // ID is returned for the published message.
+        id, err := result.Get(ctx)
+        if err != nil {
+                return fmt.Errorf("Get: %v", err)
+        }
+        fmt.Fprintf(w, "Published a message; msg ID: %v\n", id)
+        return nil
 }
